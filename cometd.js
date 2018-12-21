687b5523b20ad40b3ee32075612a48987e0bb6c3
Simone Bordet <simone.bordet@gmail.com>
2018-12-21 11:49:30 +0100

    Release 4.0.1.

--- cometd.js
+++ cometd.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-/* CometD Version 3.1.6 */
+/* CometD Version 4.0.1 */
 
 (function(root, factory) {
     if (typeof exports === 'object') {
@@ -1668,12 +1668,11 @@
 
         /**
          * Delivers the messages to the CometD server
-         * @param sync whether the send is synchronous
          * @param messages the array of messages to send
          * @param metaConnect true if this send is on /meta/connect
          * @param extraPath an extra path to append to the Bayeux server URL
          */
-        function _send(sync, messages, metaConnect, extraPath) {
+        function _send(messages, metaConnect, extraPath) {
             // We must be sure that the messages have a clientId.
             // This is not guaranteed since the handshake may take time to return
             // (and hence the clientId is not known yet) and the application
@@ -1718,7 +1717,7 @@
 
             var envelope = {
                 url: url,
-                sync: sync,
+                sync: false,
                 messages: messages,
                 onSuccess: function(rcvdMessages) {
                     try {
@@ -1745,7 +1744,7 @@
             if (_batch > 0 || _internalBatch === true) {
                 _messageQueue.push(message);
             } else {
-                _send(false, [message], false);
+                _send([message], false);
             }
         }
 
@@ -1782,7 +1781,7 @@
             var messages = _messageQueue;
             _messageQueue = [];
             if (messages.length > 0) {
-                _send(false, messages, false);
+                _send(messages, false);
             }
         }
 
@@ -1827,7 +1826,7 @@
 
                 _setStatus('connecting');
                 _cometd._debug('Connect sent', bayeuxMessage);
-                _send(false, [bayeuxMessage], true, 'connect');
+                _send([bayeuxMessage], true, 'connect');
                 _setStatus('connected');
             }
         }
@@ -1961,7 +1960,7 @@
             // so here we must bypass it and send immediately.
             _setStatus('handshaking');
             _cometd._debug('Handshake sent', message);
-            _send(false, [message], false, 'handshake');
+            _send([message], false, 'handshake');
         }
 
         function _delayedHandshake(delay) {
@@ -2661,23 +2660,14 @@
 
         /**
          * Disconnects from the Bayeux server.
-         * It is possible to suggest to attempt a synchronous disconnect, but this feature
-         * may only be available in certain transports (for example, long-polling may support
-         * it, callback-polling certainly does not).
-         * @param sync whether attempt to perform a synchronous disconnect
          * @param disconnectProps an object to be merged with the disconnect message
          * @param disconnectCallback a function to be invoked when the disconnect is acknowledged
          */
-        this.disconnect = function(sync, disconnectProps, disconnectCallback) {
+        this.disconnect = function(disconnectProps, disconnectCallback) {
             if (_isDisconnected()) {
                 return;
             }
 
-            if (typeof sync !== 'boolean') {
-                disconnectCallback = disconnectProps;
-                disconnectProps = sync;
-                sync = false;
-            }
             if (_isFunction(disconnectProps)) {
                 disconnectCallback = disconnectProps;
                 disconnectProps = undefined;
@@ -2694,7 +2684,7 @@
             _cometd._putCallback(message.id, disconnectCallback);
 
             _setStatus('disconnecting');
-            _send(sync === true, [message], false, 'disconnect');
+            _send([message], false, 'disconnect');
         };
 
         /**
